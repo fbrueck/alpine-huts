5bfd17c5bb422d0202ee1f37693db09cad59e677
fabianbrueck <fabianpunktbrueck@gmail.com>
2025-09-25 19:10:04 +0200

    feat: add transformation

---
+++ transformation/dbt/models/hut_availability.sql
@@ -0,0 +1,62 @@
+with raw_availability as (
+    SELECT hut_id, availability_data_list FROM {{source("raw_alpine_huts", "availability")}}
+),
+
+hut_info as (
+    SELECT hut_id, hut_name, latitude, longitude FROM {{ref("hut_info")}}
+),
+
+unnested as (
+    select 
+        hut_id,
+        availability_data,
+        cast(from_iso8601_timestamp(availability_data.date) as TIMESTAMP) as availability_date
+    from raw_availability
+    cross join unnest(availability_data_list) as t(availability_data)
+),
+
+projected as (
+    select
+        hut_id,
+        availability_data.hut_status,  
+        availability_data.free_beds,
+        availability_data.total_sleeping_places,
+
+        month(availability_date) as month,
+        year(availability_date) as year,
+        day_of_month(availability_date) as day,
+        availability_date,
+
+        CASE
+            WHEN day_of_week(availability_date) = 1 THEN 'Monday'
+            WHEN day_of_week(availability_date) = 2 THEN 'Tuesday'
+            WHEN day_of_week(availability_date) = 3 THEN 'Wednesday'
+            WHEN day_of_week(availability_date) = 4 THEN 'Thursday'
+            WHEN day_of_week(availability_date) = 5 THEN 'Friday'
+            WHEN day_of_week(availability_date) = 6 THEN 'Saturday'
+            WHEN day_of_week(availability_date) = 7 THEN 'Sunday'
+        END AS day_of_week_label
+
+    from unnested
+),
+
+joined as (
+    select
+        projected.hut_id,
+        hut_name,
+        latitude,
+        longitude,
+        hut_status,
+        free_beds,
+        total_sleeping_places,
+        month,
+        year,
+        day,
+        date(availability_date) as availability_date,
+        day_of_week_label
+    from projected
+    left join hut_info
+    on projected.hut_id = hut_info.hut_id
+)
+
+select * from joined;
---
+++ transformation/dbt/models/hut_info.sql
@@ -0,0 +1,53 @@
+with raw_alpine_huts as (
+    SELECT hut_id, hut_info_data FROM {{source("raw_alpine_huts", "hut_info")}}
+),
+
+renamed_alpine_huts as (
+    select 
+        hut_id,
+        hut_info_data.hut_name,
+        hut_info_data.coordinates
+    from raw_alpine_huts
+),
+
+coordinates_array as (
+    select 
+        regexp_split(coordinates, '[,|/]') as coords_array, hut_id from renamed_alpine_huts),
+
+raw_coordinates as (
+    select 
+        hut_id,
+        case 
+            when cardinality(coords_array) != 2
+            then null
+            else try_cast(trim(coords_array[1]) as double) 
+        end as latitude,
+        case 
+            when cardinality(coords_array) != 2
+            then null
+            else try_cast(trim(coords_array[2]) as double) 
+        end as longitude
+    from coordinates_array
+),
+
+coordinates_sanitized as (
+    select 
+        hut_id,
+        if(latitude > longitude, latitude, longitude) as latitude,
+        if(longitude < latitude, longitude, latitude) as longitude
+    from raw_coordinates
+    where latitude is not null and longitude is not null and latitude < 70 and latitude > 35 and longitude > 0 and longitude < 26 
+),
+
+joined as (
+    select
+        renamed_alpine_huts.hut_id,
+        hut_name,
+        latitude,
+        longitude
+    from renamed_alpine_huts 
+    left join coordinates_sanitized
+    on coordinates_sanitized.hut_id = renamed_alpine_huts.hut_id
+)
+
+select * from joined
